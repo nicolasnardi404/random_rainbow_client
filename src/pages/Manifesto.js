da394e8fbbabb0c7505e92bd3a230e79aaab36ea
esquizo ciborgue <nicolasnardi404@gmail.com>
2024-08-15 19:48:48 +0200

    write manifesto

--- src/pages/Manifesto.js
+++ src/pages/Manifesto.js
@@ -13,7 +13,29 @@ export default function Manifesto() {
         ) : (
           <HeaderUserOff />
         )}
-        <h1>ABOUT ME</h1>
+        <div className="manifesto">
+          <h1>
+            Random Rainbow is a cyber art project designed to connect queer
+            video art through a random experience. Any queer artist can join,
+            create an account, and upload up to three video links to our
+            platform.
+          </h1>
+          <h1>
+            On the homepage, you'll find a "Random" button. Each time it's
+            pressed, a video is randomly selected from our database and
+            displayed on your screen.
+          </h1>
+          <h1>
+            If you encounter any issues, feel free to reach out to us at
+            info@randomrainbow.art.{" "}
+          </h1>
+          <h1>
+            For computer nerds to know: This independent website is built
+            entirely from scratch, with Java powering the backend and JavaScript
+            with React for the frontend. If you're a fellow tech enthusiast, you
+            might appreciate the care that went into crafting this project.
+          </h1>
+        </div>
       </header>
     </div>
   );
