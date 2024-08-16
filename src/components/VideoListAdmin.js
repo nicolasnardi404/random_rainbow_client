22fd82f9bdfd16aa6fca8187eddab5dacd00ac1d
esquizo ciborgue <nicolasnardi404@gmail.com>
2024-08-16 18:07:16 +0200

    add loaidng to video list admin

--- src/components/VideoListAdmin.js
+++ src/components/VideoListAdmin.js
@@ -8,6 +8,7 @@ import "../VideoList.css"; // Import the CSS file
 const VideoList = () => {
   const history = useHistory();
   const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(true); // Loading state
   const [showAllVideos, setShowAllVideos] = useState(true);
 
   const {
@@ -16,11 +17,6 @@ const VideoList = () => {
     setAccessTokenLocal,
     setRefreshTokenLocal,
   } = useContext(AuthContext);
-
-  const headers = {
-    Authorization: `Bearer ${accessToken}`,
-    "Content-Type": "application/json",
-  };
 
   const getUpdatedToken = async () => {
     return await refreshTokenIfNeeded({
@@ -32,6 +28,7 @@ const VideoList = () => {
   };
 
   const fetchVideos = async () => {
+    setLoading(true); // Start loading
     try {
       const token = await getUpdatedToken();
       const url = showAllVideos
@@ -43,6 +40,8 @@ const VideoList = () => {
       setVideos(response.data);
     } catch (error) {
       console.error("Failed to fetch videos:", error);
+    } finally {
+      setLoading(false); // End loading
     }
   };
 
@@ -56,25 +55,30 @@ const VideoList = () => {
     );
     if (!confirmDelete) return;
 
+    setLoading(true); // Start loading
     try {
       const token = await getUpdatedToken();
       await axios.delete(
         `${process.env.REACT_APP_API_BASE_URL}/api/admin/videos/${videoId}`,
         {
-          headers: { ...headers, Authorization: `Bearer ${token}` },
+          headers: { Authorization: `Bearer ${token}` },
         }
       );
       fetchVideos();
     } catch (error) {
       console.error("Failed to delete video:", error);
+    } finally {
+      setLoading(false); // End loading
     }
   };
 
   const handleUpdate = (videoId) => {
+    setLoading(true); // Start loading
     history.push(`/admin/videos/update/${videoId}`);
   };
 
   const handleStatusChange = async (videoId, newStatus) => {
+    setLoading(true); // Start loading
     if (newStatus === "AVAILABLE") {
       const duration = prompt("Please enter the video duration (in seconds):");
       if (duration !== null && !isNaN(duration) && duration.trim() !== "") {
@@ -95,7 +99,11 @@ const VideoList = () => {
           fetchVideos();
         } catch (error) {
           console.error("Failed to update video:", error);
+        } finally {
+          setLoading(false); // End loading
         }
+      } else {
+        setLoading(false); // End loading if input is invalid
       }
     } else {
       const errorMsg = prompt("Please enter an error message for this video:");
@@ -110,7 +118,11 @@ const VideoList = () => {
           fetchVideos();
         } catch (error) {
           console.error("Failed to update video status:", error);
+        } finally {
+          setLoading(false); // End loading
         }
+      } else {
+        setLoading(false); // End loading if input is invalid
       }
     }
   };
@@ -137,56 +149,64 @@ const VideoList = () => {
       <h1 className="special-title">
         {showAllVideos ? "ALL VIDEOS" : "REVIEW VIDEOS"}
       </h1>
-      <div className="video-list-cards">
-        {videos.map((video) => (
-          <div key={video.id} className="video-card">
-            <h2 className="video-title">{video.title}</h2>
-            <p className="video-username">{video.user.username}</p>
-            <p>
-              <select
-                className="video-select"
-                value={video.videoStatus}
-                onChange={(e) => handleStatusChange(video.id, e.target.value)}
-              >
-                <option value="AVAILABLE">Available</option>
-                <option value="ERROR">Error</option>
-                <option value="DOESNT_RESPECT_GUIDELINES">
-                  Doesn't Respect Guidelines
-                </option>
-                <option value="UNCHECKED">Unchecked</option>
-              </select>
-            </p>
-            <p>
-              <h4 className="video-error">
-                {video.messageError ? `Error: ${video.messageError}` : ""}
-              </h4>
-            </p>
-            <p>
-              <a
-                href={`http://www.randomrainbow.art/home/${video.endpoint}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                View Video
-              </a>
-            </p>
-            <div className="video-actions">
-              <button
-                className="default-btn update-btn"
-                onClick={() => handleUpdate(video.id)}
-              >
-                Update
-              </button>
-              <button
-                className="default-btn delete-btn"
-                onClick={() => handleDelete(video.id)}
-              >
-                Delete
-              </button>
+      {loading ? ( // Show loading indicator when loading is true
+        <div className="special-title" style={{ border: "none" }}>
+          Loading...
+        </div>
+      ) : videos.length === 0 ? ( // Check if there are no videos to display
+        <div className="title">No videos to review</div>
+      ) : (
+        <div className="video-list-cards">
+          {videos.map((video) => (
+            <div key={video.id} className="video-card">
+              <h2 className="video-title">{video.title}</h2>
+              <p className="video-username">{video.user.username}</p>
+              <p>
+                <select
+                  className="video-select"
+                  value={video.videoStatus}
+                  onChange={(e) => handleStatusChange(video.id, e.target.value)}
+                >
+                  <option value="AVAILABLE">Available</option>
+                  <option value="ERROR">Error</option>
+                  <option value="DOESNT_RESPECT_GUIDELINES">
+                    Doesn't Respect Guidelines
+                  </option>
+                  <option value="UNCHECKED">Unchecked</option>
+                </select>
+              </p>
+              <p>
+                <h4 className="video-error">
+                  {video.messageError ? `Error: ${video.messageError}` : ""}
+                </h4>
+              </p>
+              <p>
+                <a
+                  href={`http://www.randomrainbow.art/home/${video.endpoint}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View Video
+                </a>
+              </p>
+              <div className="video-actions">
+                <button
+                  className="default-btn update-btn"
+                  onClick={() => handleUpdate(video.id)}
+                >
+                  Update
+                </button>
+                <button
+                  className="default-btn delete-btn"
+                  onClick={() => handleDelete(video.id)}
+                >
+                  Delete
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
       <button className="default-btn special-btn" onClick={toggleVideoList}>
         {showAllVideos ? "GO TO REVIEW VIDEOS" : "GO TO ALL VIDEOS"}
       </button>
