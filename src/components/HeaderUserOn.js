2a50d0a74dfcdf682c47adcb3a914e6ad9137e24
esquizo ciborgue <nicolasnardi404@gmail.com>
2024-11-02 20:29:39 +0100

    add avatar

--- src/components/HeaderUserOn.js
+++ src/components/HeaderUserOn.js
@@ -2,6 +2,9 @@ import React, { useState, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import { AuthContext } from "./AuthContext";
 import logo from "../images/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSmile } from "@fortawesome/free-solid-svg-icons";
+import "../styles/Header.css";
 
 export default function HeaderUserOn() {
   const { username, setAccessTokenLocal } = useContext(AuthContext);
@@ -19,6 +22,15 @@ export default function HeaderUserOn() {
 
   return (
     <div>
+      <div className="user-avatar">
+        <div
+          className="icons-style menu-btn"
+          onClick={() => handleClick("videos")}
+        >
+          <FontAwesomeIcon icon={faSmile} />
+          <h3>{username}</h3>
+        </div>
+      </div>
       <img src={logo} alt="RANDOM RAINBOW" className="logo-title" />
       {/* Mobile Menu Toggle Button */}
       <button className="menu-button" onClick={toggleMenu}>
